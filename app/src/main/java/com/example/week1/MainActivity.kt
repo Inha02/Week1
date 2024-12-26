5876271225915b57aa02effc8149a69041426c56
sejun <sejun0601@naver.com>
2024-12-26 16:05:37 +0900

    second commit

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.week1.ui.theme.Week1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,11 +33,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Week1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface () {
+                    ContactView()
                 }
             }
         }
@@ -32,16 +42,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ContactView(name: String, modifier: Modifier = Modifier) {
-    LazyColumn (modifier = ){
+fun ContactView() {
 
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = Color.Cyan,
+                contentColor = Color.White,
+                onClick = {}
+            ) {
+            }
+        }
+    ) { innerpadding ->
+        Column (modifier = Modifier.padding(innerpadding)){
+
+            LazyColumn (modifier = Modifier.fillMaxSize()){
+                items(10){
+                    Surface (
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        shadowElevation = 4.dp
+                    ){
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp),
+                        ) {
+
+
+                        }
+                    }
+                }
+            }
+        }
     }
+
+
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Week1Theme {
-        Greeting("Android")
+        ContactView()
     }
 }
